a2a6b0a946599ee48d0ae51bbb1271d2dd97310d
George Zubrienko <gzu@ecco.com>
2022-11-02 12:05:50 +0100

    Spark 3.3.1 (#53)

--- test/conftest.py
+++ test/conftest.py
@@ -4,6 +4,10 @@ from pyspark.sql import SparkSession
 
 @pytest.fixture(scope="session")
 def spark_session():
-    session = SparkSession.builder.getOrCreate()
+    java_17_launch_options = "-XX:+IgnoreUnrecognizedVMOptions --add-opens=java.base/java.lang=ALL-UNNAMED --add-opens=java.base/java.lang.invoke=ALL-UNNAMED --add-opens=java.base/java.lang.reflect=ALL-UNNAMED --add-opens=java.base/java.io=ALL-UNNAMED --add-opens=java.base/java.net=ALL-UNNAMED --add-opens=java.base/java.nio=ALL-UNNAMED --add-opens=java.base/java.util=ALL-UNNAMED --add-opens=java.base/java.util.concurrent=ALL-UNNAMED --add-opens=java.base/java.util.concurrent.atomic=ALL-UNNAMED --add-opens=java.base/sun.nio.ch=ALL-UNNAMED --add-opens=java.base/sun.nio.cs=ALL-UNNAMED --add-opens=java.base/sun.security.action=ALL-UNNAMED --add-opens=java.base/sun.util.calendar=ALL-UNNAMED --add-opens=java.security.jgss/sun.security.krb5=ALL-UNNAMED --add-opens=java.base/java.util.stream=ALL-UNNAMED"
+    session = SparkSession.builder \
+        .config("spark.driver.extraJavaOptions", java_17_launch_options) \
+        .config("spark.executor.extraJavaOptions", java_17_launch_options) \
+        .getOrCreate()
     yield session
     session.stop()
